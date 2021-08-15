cc6d76f1413b18490d129da195696a104eeb00b0
cfrankovich <carsonfrankovich6@gmail.com>
2021-08-15 01:11:50 -0400

    Line counter working

--- linje.pl
+++ linje.pl
@@ -1,17 +1,56 @@
 #!/usr/bin/env perl
 
+use strict;
+use warnings;
+
 use Cwd qw(cwd getcwd);
 my $dir = cwd;
-@FILES = defined $ARGV[0] ? @ARGV : glob($dir . '/*');
+our @FILES = defined $ARGV[0] ? @ARGV : glob($dir . '/*');
 
-$linecount = 0;
-foreach $i (@FILES)
+our $totallinecount = 0;
+our $totalblankcount = 0;
+our $totalcodecount = 0;
+
+print("= - - - - - - - - - - - - - - - - - - - - - - - - - - - - =\n");
+print("  File\t\tLines\t\tBlank\t\tCode\t\t\n");
+print("= - - - - - - - - - - - - - - - - - - - - - - - - - - - - =\n");
+
+foreach my $i (@FILES)
 {
+	my $lc = 0;
+	my $bc = 0;
+	my $cc = 0;
 	open(FILE, "<$i") or die "Couldn't open file $!";
-	while (<FILE>)
+	if (-d $i) { next; }
+	foreach my $line (<FILE>)
 	{
-		$linecount++;
+		if (defined $line)
+		{
+			$totalcodecount++;
+			$cc++;
+		}
+		else
+		{
+			$totalblankcount++;
+			$bc++;
+		}
+		$totallinecount++;
+		$lc++;
 	}
+	my @splitty= split('/', $i);
+	my $filename = $splitty[scalar @splitty - 1];
+	if (length($filename) < 6)
+	{
+		print("  $filename\t\t$lc\t\t$bc\t\t$cc\n");
+	}
+	else
+	{
+		print("  $filename\t$lc\t\t$bc\t\t$cc\n");
+	}
+
 }
 
-print ("Linecount: $linecount\n");
+print("= - - - - - - - - - - - - - - - - - - - - - - - - - - - - =\n");
+print("  Total\t\t$totallinecount\t\t$totalblankcount\t\t$totalcodecount\n");
+print("= - - - - - - - - - - - - - - - - - - - - - - - - - - - - =\n");
+
